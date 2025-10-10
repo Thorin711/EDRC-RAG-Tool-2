c40996063d267eeb41a519b724823eb6e709c06e
Thorin711 <107803601+Thorin711@users.noreply.github.com>
2025-10-10 21:47:48 +0100

    Fix: file paths 4

--- app.py
+++ app.py
@@ -482,7 +482,7 @@ def main():
                     authors = doc.metadata.get('authors', 'No Authors Found')
                     source_path = doc.metadata.get('source', 'Unknown Source')
                     # Get the base filename and remove the .md extension.
-                    base_name = source_path.split("\")[-1]
+                    base_name = source_path.split("\\")[-1]
                     source = base_name.split('.')[0]
                     year = doc.metadata.get('year', 'Unknown Year')
                     doi = doc.metadata.get('doi', '')
